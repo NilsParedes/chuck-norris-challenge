595cf616f2c038914884a71aa5d6d2ae960b8195
Nils Paredes <nils.parsa@gmail.com>
2022-09-04 22:55:57 -0500

    Minor fixes

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -3,7 +3,5 @@ package ports
 import "github.com/nilsparedes/chuck-norris-challenge/internal/core/domain"
 
 type JokeService interface {
-	// Testeando
-	// Testing
 	Fetch() ([]domain.Joke, error)
 }
